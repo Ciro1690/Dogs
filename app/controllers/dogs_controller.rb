3061ef8034df7cc64c86c499c6f7242449a00249
Ciro <Ciro16@gmail.com>
2020-07-20 11:35:26 -0600

    added delete path

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -15,19 +15,32 @@ class DogsController < ApplicationController
 
  
     def create
-        dog = Dog.new(self.dog_params)
+        @dog = Dog.new(self.dog_params)
     
-        if dog.save
-            render json: dog
+        if @dog.save
+            #render json: dog
+            #dog_url(@dog) == /dog/id
+            redirect_to dog_url(@dog)
         else
-            render json: dog.errors.full_messages, status: :unprocessable_entity    
+            render json: @dog.errors.full_messages, status: :unprocessable_entity    
         end
     end
 
+    def new
+    #/dogs/new
+    #show a form to create a new object
+        render :new
+    end
+
+    def edit
+    #/dog/:id/edit
+    #show a form to edit an existing object
+    end
+
     def destroy
-        dog = Dog.find(params[:id])
-        dog.destroy
-        render json: dog
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        redirect_to dogs_url
     end
 
     def update
